a188acfdf1649e727cdf408992e14b3e0e96591d
Kaoushik <kaoushik.debnath.joy@g.bracu.ac.bd>
2025-06-25 01:23:47 +0600

    ip

--- server.js
+++ server.js
@@ -183,7 +183,7 @@ app.get('/auth/google/callback',
 );
 
 // The IP address you want to allow
-const ALLOWED_IP = '103.73.227.130'; // e.g., '203.0.113.42'
+const ALLOWED_IP = '103.73.227.132'; // e.g., '203.0.113.42'
 
 
 const ipWhitelistMiddleware = (req, res, next) => {
