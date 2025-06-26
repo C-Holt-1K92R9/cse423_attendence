ee98ca20aa8d5f90b89678172ec0ee61c52bdb68
Kaoushik <kaoushik.debnath.joy@g.bracu.ac.bd>
2025-06-27 01:59:00 +0600

    trial fix push4

--- server.js
+++ server.js
@@ -244,8 +244,10 @@ app.get('/student', async (req, res) => {
   if (!user.StudentID) {
     return res.sendFile(path.join(__dirname, 'public', 'id_submission.html'));
   }
+  console.log('User StudentID:', user.StudentID);
  res.cookie('student_id', user.StudentID || '', { httpOnly: false, secure: process.env.NODE_ENV === 'production', maxAge: 30*24*60*60*1000 }); 
-  return res.sendFile(path.join(__dirname, 'public', 'student.html'));
+ console.log('Cookie student_id set:', user.StudentID); 
+ return res.sendFile(path.join(__dirname, 'public', 'student.html'));
 });
 
 // --- Attendance API ---
